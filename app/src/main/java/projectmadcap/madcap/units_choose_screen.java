c9445080ff0c34057e2bb5141e91e17b1ba907d8
John P. Baggs <johnpbaggs@gmail.com>
2016-04-02 21:34:25 -0500

    Jp's sexy 10th
    added activity .xml files for units choose screen and behaviors choose screen. Fix images

---
+++ app/src/main/java/projectmadcap/madcap/units_choose_screen.java
@@ -0,0 +1,13 @@
+package projectmadcap.madcap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class units_choose_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_units_choose_screen);
+    }
+}
